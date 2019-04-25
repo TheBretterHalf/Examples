87ac73b027602e40a181ad061cc8d0084af5b117
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-25 23:56:26 +0000

    continued lessons

--- Week1Examples/test.c
+++ Week1Examples/test.c
@@ -5,19 +5,21 @@
 
 //int auto returns itself
 //void similar to null, not sending any information
-int main (void)
-{
-    //print format
-    //needs quotes not single quotes
-    //single quotes will send ascii value to the code
-    //clang compiles the language, makes an a.out file
-    //\ escapes the flow of the code and overides the standard behavior of that code
-    //so the \n escapes the flow of the "" and allows you to access the property of the n character
-    //make does both
-    printf("Hello, World\n");
-}
-// int main(void)
+// int main (void)
 // {
-//     string name = get_string("What is your name?\n");
-//     printf("hello, %s\n", name);
+//     //print format
+//     //needs quotes not single quotes
+//     //single quotes will send ascii value to the code
+//     //clang compiles the language, makes an a.out file
+//     //\ escapes the flow of the code and overides the standard behavior of that code
+//     //so the \n escapes the flow of the "" and allows you to access the property of the n character
+//     //make does both
+//     //clang test.c -o hello will allow you to customize the output
+//     //make -[file name] will return the excecutable code
+//     printf("Hello, World\n");
 // }
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
+}
