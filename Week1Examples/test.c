04e79d813313650fbda5de22948f7c63f7d39b2a
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-26 00:09:25 +0000

    coding exercise

--- Week1Examples/test.c
+++ Week1Examples/test.c
@@ -18,11 +18,20 @@
 //     //make -[file name] will return the excecutable code
 //     printf("Hello, World\n");
 // }
-int main(void)
+// int main(void)
+// {
+//     //% a placeholder s is identifying it as a string
+//     //order of the percent symbols is the order of the variables trailing
+//     //common syntax of c uses small case as a standard but uses camel case going forward
+//     string name = get_string("What is your name?\n");
+
+//     printf("hello, %s\n", name);
+// }
+
+int main (void)
 {
-    //% a placeholder s is identifying it as a string
-    //order of the percent symbols is the order of the variables trailing
-    //common syntax of c uses small case as a standard but uses camel case going forward
-    string name = get_string("What is your name?\n");
-    printf("hello, %s\n", name);
+    string firstname = get_string("What is your first name?\n");
+    string lastname = get_string("What is your last name?\n");
+
+    printf("Hello, %s %s\n", firstname, lastname);
 }
