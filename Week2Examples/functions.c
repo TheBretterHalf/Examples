c1d9e29afd05ed84ab4ec46f76a6f252af00e79e
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-08 23:54:42 +0000

    Functionge

---
+++ Week2Examples/functions.c
@@ -0,0 +1,9 @@
+int getItemIndex(int item, int rmin)
+{
+    return item - rmin;
+}
+int decrypt(int iItem, key)
+{
+    return iItem + Key
+}
+(decrypt(getItemIndex(item, rmin),key) % imin + 1 + rmin
