41875b22f1a852c03c104c498d2b1ef2b6889ae2
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-15 22:42:32 +0000

    Week3Ex

--- Week2Examples/functions.c
+++ Week2Examples/functions.c
@@ -1,24 +1,45 @@
-int getItemIndex(int item, int rmin)
-{
-    return item - rmin;
-}
-int encrypt(int iItem, key)
-{
-    return iItem + Key
-}
-// int getMod (int imin)
+// int getItemIndex(int item, int rmin)
 // {
-//     return imin + 1
+//     return item - rmin;
 // }
+// int encrypt(int iItem, key)
+// {
+//     return iItem + Key
+// }
+// // int getMod (int imin)
+// // {
+// //     return imin + 1
+// // }
 
-int itemIndex = getItemIndex(item, rmin);
-printf("Item Index: %i\n", itemIndex)
-//item index should be 17
-int encryptedKey = encrypt(itemIndex, key);
-int modifier = imin + 1;
-int returnToRange = rMin;
-int convertedIndex = decrypted % modifier
+// int itemIndex = getItemIndex(item, rmin);
+// printf("Item Index: %i\n", itemIndex)
+// //item index should be 17
+// int encryptedKey = encrypt(itemIndex, key);
+// int modifier = imin + 1;
+// int returnToRange = rMin;
+// int convertedIndex = decrypted % modifier
 
-convertedIndex + returnToRange
+// convertedIndex + returnToRange
 
-//(decrypt(getItemIndex(item, rmin),key) % imin + 1 + rmin
+// //(decrypt(getItemIndex(item, rmin),key) % imin + 1 + rmin
+
+#include <cs50.h>
+#include <stdio.h>
+
+//function must be declared before it is used
+//name of the function, return type, and what parameters the function will take
+int total (int a, int b, int c);
+
+int main(void)
+{
+    printf("Hello\n");
+    int result = total(5, 8, 2);
+    printf("%i\n", result);
+    //writing an item in-line
+    printf("%i\n", total(4,4,4));
+}
+
+int total (int a, int b, int c)
+{
+    return a;
+}
---
+++ Week3Examples/Readme.txt
@@ -0,0 +1,11 @@
+Week 3
+
+Typedot result
+
+{
+byte rgbt RED
+byte rgbt Blue
+byte rgbt Green
+} RGBTRIPLE
+
+byte is less data than int
