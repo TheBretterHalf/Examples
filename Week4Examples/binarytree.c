69bf07d69b5e7774029db7be9328ddaf01f83453
Brett Stainken <brettstainken.student@careerdevs.com>
2019-06-07 01:24:29 +0000

    binarytree

--- Week4Examples/binarytree.c
+++ Week4Examples/binarytree.c
@@ -4,11 +4,39 @@
 typedef struct NODE
 {
     int value;
-    struct NODE *left;
-    struct NODE *right;
+    //you have to add struct because the typedef has not finished
+    struct NODE *low;
+    struct NODE *high;
 } NODE;
+
+NODE *rootNode;
+
+void addNode (int value)
+{
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    newNode->high = NULL;
+    newNode->low = NULL;
+
+    if (rootNode == NULL)
+    {
+        rootNode = newNode;
+        return;
+    }
+}
+
+//
 
 int main (void)
 {
     //printf("hello\n");
+    addNode(10);
+    addNode(8);
+    addNode(15);
+    addNode(17);
+    addNode(19);
+    addNode(20);
 }
+
+//MVP, get 8 and 15 working.
+//get others working as well to
