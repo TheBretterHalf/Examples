377d046435ef83839efbc9b543446b12a36042d6
Brett Stainken <brettstainken.student@careerdevs.com>
2019-06-10 23:17:11 +0000

    binarytree

--- Week4Examples/binarytree.c
+++ Week4Examples/binarytree.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define true 1
+#define false 0
+
 typedef struct NODE
 {
     int value;
@@ -23,12 +26,31 @@ void addNode (int value)
         rootNode = newNode;
         return;
     }
+
+    //if value of new node is less the root node attach to low
+    //else add to high
+    //input while loop to confirm NULL
+    while(1)
+    {
+        if (newNode->value < rootNode->value)
+        {
+            rootNode->low = newNode;
+            return;
+        }
+        else
+        {
+            rootNode->high = newNode;
+            return;
+        }
+
+    }
 }
 
 void displayTreeOnEnter(NODE *curNode)
 {}
 
 void displayTreeOnDeparture(NODE *curNode)
+{}
 //hashtable
 
 int main (void)
@@ -37,9 +59,19 @@ int main (void)
     addNode(10);
     addNode(8);
     addNode(15);
+    // addNode(17);
+    // addNode(19);
+    // addNode(20);
     addNode(17);
-    addNode(19);
     addNode(20);
+    addNode(18);
+    addNode(5);
+    addNode(6);
+    addNode(9);
+    addNode(4);
+    addNode(13);
+    addNode(7);
+
 }
 
 //MVP, get 8 and 15 working.
